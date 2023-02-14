f44d0868c8c9683ee53e2faf02be6c33ecdcd987
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2023-02-13 20:34:48 -0800

    Add project files.

---
+++ BanoDoctor.Admin.UI/Domains/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanoDoctor.Admin.UI.Domains
+{
+    public class BaseModel<T>
+    {
+        public T Id { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ BanoDoctor.UI/Controllers/AboutUsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanoDoctor.UI.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/AboutUs/AboutUs.cshtml");
+        }
+    }
+}
